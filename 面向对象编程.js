ffb1e84d775b40b86ed0ae046fa6851e99878574
JimmyFromMBP <zfmmpx@gmail.com>
2018-07-04 00:12:35 +0800

    去掉了所有mac上的vocode不可见的bs控制字符

--- 面向对象编程.js
+++ 面向对象编程.js
@@ -1,10 +1,4 @@
-function Cat(name, color) {
-    this.name = name;
-    this.color = color;
-  }
-  var cat1 = new Cat("大毛", "黄色");
-  var cat2 = new Cat("二毛", "黑色");
 
-  console.log(Cat.prototype.isPrototypeOf(cat1));
-  console.log(Function.prototype.isPrototypeOf(cat1));
-  console.log(Object.prototype.isPrototypeOf(cat1));
+var a = "\u200b"
+console.log(a);
+console.log(" ");
