e7a10752d61d8e76f3b156f904a81b6a4fcb6624
unknown <huangjian@ways.cn>
2018-07-05 09:55:54 +0800

    07050955

---
+++ source/_posts/0705-学习ES6：Class的继承.md
@@ -0,0 +1,57 @@
+---
+title: 学习ES6：Class的继承
+comments: true
+categories:
+  - 前端
+  - CSS
+  - CSS一般性重要概念
+tags:
+  - 我的笔记
+language:
+  - zh-cn
+date: 2018-07-05 09:31:31
+updated: 2018-07-05 09:31:31
+---
+
+super既可当做函数调用，又可当做对象使用
+
+
+一、super作为函数调用时，代表父类的构造函数，
+```js
+class A{}
+
+class B extends A {
+  constructor() {
+    super();
+  }
+}
+```
+虽然代表了父类A的构造函数，但是返回的是子类B的实例，即super内部的this指的是B，这里的super()相当于`A.prototype.constructor.call(this)`。
+而且作为函数时，只能用在子类的构造函数之中，用在其他地方就会报错。
+
+二、super作为对象时，在普通方法中，指向父类的原型对象；在静态方法中，指向父类。
+ES6 规定，在子类普通方法中通过super调用父类的方法时，方法内部的this指向当前的子类实例。
+```js
+class A {
+  constructor() {
+    this.x = 1;
+  }
+  print() {
+    console.log(this.x);
+  }
+}
+
+class B extends A {
+  constructor() {
+    super();
+    this.x = 2;
+  }
+  m() {
+    super.print();
+  }
+}
+
+let b = new B();
+b.m() // 2
+```
+上面代码中，super.print()虽然调用的是A.prototype.print()，但是A.prototype.print()内部的this指向子类B的实例，导致输出的是2，而不是1。也就是说，实际上执行的是super.print.call(this)。
--- test1.js
+++ test1.js
@@ -1,83 +1,12 @@
-// 现在有一个“动物”对象的构造函数
-function Animal() {
-    this.species = "动物";
-}
-
-// 还有一个“猫”对象的构造函数
-function Cat(name, color){
-    this.name = name;
-    this.color = color;
-}
-
-
-// 怎样才能使“猫”继承“动物”呢？
-
-
-
-// 一、构造函数绑定
-function Cat(name, color){
-    Animal.apply(this, arguments);
-    this.name = name;
-    this.color = color;
-}
-var cat1 = new Cat("大毛", "黄色");
-console.log(cat1.species); // 动物
-// 一、构造函数绑定
-
-
-
-// 二、prototype模式，
-Cat.prototype = new Animal();
-Cat.prototype.constructor = Cat;
-var cat1 = new Cat("大毛", "黄色");
-console.log(cat1.species); // 动物
-// 二、prototype模式，
-
-
-
-// 三、直接继承prototype
-function Animal(){ }
-Animal.prototype.species = "动物";
-Cat.prototype = Animal.prototype;
-Cat.prototype.constructor = Cat;
-var cat1 = new Cat("大毛", "黄色");
-console.log(cat1.species);
-// 三、直接继承prototype
-
-
-
-// 四、利用空对象作为中介
-function Animal(){ }
-Animal.prototype.species = "动物";
-var F = function(){};
-F.prototype = Animal.prototype;
-Cat.prototype = new F();
-Cat.prototype.constructor = Cat;
-var cat1 = new Cat("大毛", "黄色");
-console.log(cat1.species);
-// 四、利用空对象作为中介
-
-
-
-
-
-
-// 比如，现在有一个对象，叫做"中国人"。
-var Chinese = {
-    nation:'中国'
-  };
-  var Doctor ={
-    career:'医生'
+class A {
+    constructor() {
+      console.log(new.target.name);
+    }
   }
-  // 这里要注意，这两个对象都是普通对象，不是构造函数，无法使用构造函数方法实现"继承"。
-  
-
-　　function object(o) {
-
-    　　　　function F() {}
-    
-    　　　　F.prototype = o;
-    
-    　　　　return new F();
-    
-    　　}
+  class B extends A {
+    constructor() {
+      super();
+    }
+  }
+  new A() // A
+  new B() // B
